ef17ad7e4b99099cd7cedc1d318826c9973ce55a
mileprogramer <obecano.ispunjeno@gmail.com>
2024-06-23 18:13:55 +0200

    Refactor the pagination and add validation

--- Services/Paginate.js
+++ Services/Paginate.js
@@ -1,16 +1,41 @@
+const HttpError = require("../Errors/HttpError");
+
 class Paginataion{
 
     static make(dataToPaginate, page, perPage)
     {
+        page = this.validate(page);
         let startIndex = perPage * page;
-        return dataToPaginate.filter((element, index) =>{
+        let data = dataToPaginate.filter((element, index) =>{
             if(index >= startIndex && perPage !== 0){
                 perPage--;
                 return element;
             }
         })
+        if(data.length === 0){
+            throw new HttpError({
+                "status": 404,
+                "message": "There is not such a page",
+            });
+        }
+        return data;
     }
 
+    static validate(page)
+    {
+        if(!page){
+            page = 0;
+        } else if(page > 0){
+            page = parseInt(page) - 1;
+        }
+        else {
+            throw new HttpError({
+                "status": 404,
+                "message": "There is not such a page",
+            })
+        }
+        return page;
+    }
 
 }
 
