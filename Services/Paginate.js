361a8946a5291283d099ec8f624148f1e4ceb8a8
mileprogramer <obecano.ispunjeno@gmail.com>
2024-06-27 19:44:32 +0200

    Refactor paginate to it can handle mistakes in catch blocks

--- Services/Paginate.js
+++ Services/Paginate.js
@@ -1,55 +1,45 @@
 const HttpError = require("../Errors/HttpError");
 const DB = require("../database/DB");
-
+const CarModel = require("../models/CarModel")
 class Paginataion{
 
-    static async make(collection, page, perPage)
+    static async make({modelToCall, getTotal, collection, page, perPage})
     {
-        try{
-            page = this.validatePage(page);
-        }
-        catch (error){
-            return error;
-        }
-        try{
-            let dataToPaginate = [];
-            let totalDataToPaginate = 0;
-            let startNumber = (page - 1) * perPage;
-            let endNumber = startNumber + perPage;
-            console.log(startNumber, endNumber);
-            [dataToPaginate, totalDataToPaginate] = await Promise.all([
-                DB.get(collection, startNumber, endNumber),
-                DB.getTotal(collection)
-            ]);
-            if(dataToPaginate.length === 0){
-                throw new HttpError({
-                    "status": 404,
-                    "message": "This page does not exists"
-                })
-            }
-            return {dataToPaginate, totalDataToPaginate};
-        }catch (httpError){
-            console.log("Error in pagination make on collection", collection);
-            console.log(httpError);
-            return httpError;
-        }
-    }
-    static validatePage(page)
-    {
+        // check is user provided right page
         page = parseInt(page);
         if(!page){
-            page = 1;
-        } else if(page > 0){
-            return page;
+            throw new HttpError({
+                status: 404,
+                message: "There is not such a page"
+            });
         }
-        else {
+
+        let dataToPaginate = [];
+        let totalDataToPaginate = 0;
+        let startNumber = (page - 1) * perPage;
+        let endNumber = startNumber + perPage;
+
+        try{
+            [dataToPaginate, totalDataToPaginate] = await Promise.all([
+                modelToCall(collection, startNumber, endNumber),
+                getTotal(collection)
+            ]);
+        }
+        catch (httpError){
+            console.log("Error in pagination make on collection", collection);
+            console.log(httpError);
+            throw httpError;
+        }
+
+        if(dataToPaginate.length === 0){
             throw new HttpError({
                 "status": 404,
-                "message": "There is not such a page",
+                "message": "This page does not exists"
             })
         }
-    }
 
+        return {dataToPaginate, totalDataToPaginate};
+    }
 }
 
 module.exports = Paginataion;
