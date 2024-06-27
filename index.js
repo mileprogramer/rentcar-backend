42f815f7bb28b0380534a13805847e0ea8fb9a1f
mileprogramer <obecano.ispunjeno@gmail.com>
2024-06-27 15:35:25 +0200

    Add mongo db connection

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
+const PORT = 3030;
 const routesCars = require("./routes/cars")
 const cors = require("cors");
+const DB = require("./database/DB");
+
+// connect to the db
+DB.connect("mongodb://localhost:27017")
+    .then(()=>{
+        console.log("You succesfully connected to the DB");
+    })
+    .catch((errors)=>{
+        console.log("Error happened with connection to the db", errors);
+    })
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/cars", routesCars);
 
-app.listen(3030, (err)=>{
-    if(err) return console.log("POSTOJI GRESKA");
-    console.log("SERVER JE POKRENUT");
+app.listen(PORT, (err)=>{
+    if(err) return console.log("Error Exist");
+    console.log("SERVER IS WORKING ON "+ PORT);
 })
